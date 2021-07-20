62a756e47a7209f154699c0789dad76d6f28a148
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-07-19 22:38:16 -0500

    made corrections to javascript

--- jaren.js
+++ jaren.js
@@ -3,66 +3,68 @@ console.log('js file connected');
 
 
 function myFunctionYes() {
-  alert("Congrats! We're friends." );
+  alert("Congrats! We're friends.");
 }
 function myFunctionNo() {
-  alert("We're not friends." );
+  alert("We're not friends.");
 }
 
-function yourFavoriteTeam() {
-var yourFavoriteTeam =
-document.getElementById("yourTeam").value;
-var text;
+  var text;
 
   // If the team is "Cubs"
-  if(yourFavoriteTeam === "Cubs"){
-  text = "Go Cubs!";
+  if (yourFavoriteTeam === "Cubs") {
+    text = "Go Cubs!";
 
-  //If the team is anything else
-} else {
-  text = "That team is Terrible!";
-}
-document.getElementById("yourTeam").innerHTML = text;
+    //If the team is anything else
+  } else {
+    text = "That team is Terrible!";
+  }
+  document.getElementById("yourTeam").innerHTML = text;
 }
 
 //use prompt and confirm methods
-function questionSix(){
-console.log("question six function works");
-var userTeam = document.getElementById('yourTeam');
-console.log(userTeam.value);
-var userResponse = userTeam.value.toLowerCase();
+function questionSix() {
+  console.log("question six function works");
+  var userTeam = document.getElementById('yourTeam');
+  console.log(userTeam.value);
+  var userResponse = userTeam.value.toLowerCase();
 
-if(userResponse === 'cubs'){
-var userGame = confirm('They are the best, click ok to win your prize.');
-console.log(userGame);
+  if (userResponse === 'cubs') {
+    var userGame = confirm('They are the best, click ok to win your prize.');
+    console.log(userGame);
 
-if(userGame === true){
-console.log('We are playing the game.');
-
-
-var numTeams = 2;
-var otherTeams = 'Tell me what other teams you like.';
-
-while(numTeams > 0){
-var userInput = prompt(otherTeams + 'You have ' + numTeams + ' turns left.');
-if(userInput !== ""){
-  alert('Not a fan..');
-  numTeams = numTeams - 1;
-}
+    if (userGame === true) {
+      console.log('We are playing the game.');
 
 
+      var numTeams = 2;
+      var otherTeams = 'Tell me what other teams you like.';
 
-}//closes while loop
+      while (numTeams > 0) {
+        var userInput = prompt(otherTeams + 'You have ' + numTeams + ' turns left.');
+        if (userInput !== "") {
+          if (user input === "cubs") {
+          alert("That's my favorite team!");
+          break;
+        } else {
+          alert('Not a fan..')
+          numTeams = numTeams - 1;
+        }
+      }
 
 
-}//closes if user game is true
+
+      }//closes while loop
 
 
-} else {
-alert('No that is not the right team.');
+    }//closes if user game is true
 
-}//closes main if statement
 
-alert('Thanks for playing my game.');
+  } else {
+    alert('No that is not the right team.');
+
+  }//closes main if statement
+
+  alert('Thanks for playing my game.');
 
 }//closes question six function
